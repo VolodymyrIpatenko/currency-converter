967e0b550b743cd60738f98efbb0f6ef3dc79820
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2023-03-29 11:19:46 +0300

    adding styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Dropdown from 'react-dropdown';
 import { HiSwitchHorizontal } from 'react-icons/hi';
 import 'react-dropdown/style.css';
-import './App.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { MainSection, CurrenciesContainer, Input, ConvertBtn, ResultContainer } from './App.styled';
 
 function App() {
-  const [info, setInfo] = useState([]);
+  const [exchangeRates, setExchangeRates] = useState([]);
   const [input, setInput] = useState(0);
   const [from, setFrom] = useState('usd');
   const [to, setTo] = useState('uah');
   const [currencies, setCurrencies] = useState([]);
   const [output, setOutput] = useState(0);
 
-  const handleInput = e => {
+  const handleInput = (e) => {
     let userValue = e.target.value;
     userValue = userValue
       .split('')
-      .filter(el => el.trim())
+      .filter((el) => el.trim())
       .join('');
     if (Number.isNaN(Number(userValue))) {
       toast.error('Please enter a number');
@@ -29,21 +29,19 @@ function App() {
 
   useEffect(() => {
     axios
-      .get(
-        `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`
-      )
-      .then(res => {
-        setInfo(res.data[from]);
+      .get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`)
+      .then((res) => {
+        setExchangeRates(res.data[from]);
       });
   }, [from]);
 
   useEffect(() => {
-    setCurrencies(Object.keys(info));
+    setCurrencies(Object.keys(exchangeRates));
     convert();
-  }, [info]);
+  }, [exchangeRates]);
 
   function convert() {
-    let rate = info[to];
+    let rate = exchangeRates[to];
     setOutput(input * rate);
   }
 
@@ -54,65 +52,62 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <MainSection>
       <ToastContainer />
-      <div className="heading">
-        <h1>Currency converter</h1>
-      </div>
-      <div className="container">
-        <div className="left">
+      <h1>Currency converter</h1>
+      <CurrenciesContainer>
+        <div>
           <h3>Amount</h3>
-          <input
-            type="text"
-            placeholder="Enter the amount"
-            onChange={e => handleInput(e)}
-          />
+          <Input type="text" placeholder="Enter the amount" onChange={(e) => handleInput(e)} />
         </div>
-        <div className="middle">
+        <div>
           <h3>From</h3>
           <Dropdown
             options={currencies}
-            onChange={e => {
+            onChange={(e) => {
               setFrom(e.value);
             }}
             value={from}
             placeholder="From"
           />
         </div>
-        <div className="switch">
-          <HiSwitchHorizontal
-            size="30px"
-            onClick={() => {
-              flip();
-            }}
-          />
-        </div>
-        <div className="right">
+        <HiSwitchHorizontal
+          style={{
+            marginTop: '30px',
+            backgroundColor: 'forestgreen',
+            borderRadius: '50%',
+            padding: '3px',
+            cursor: 'pointer'
+          }}
+          size="30px"
+          onClick={() => {
+            flip();
+          }}
+        />
+        <div>
           <h3>To</h3>
           <Dropdown
             options={currencies}
-            onChange={e => {
+            onChange={(e) => {
               setTo(e.value);
             }}
             value={to}
             placeholder="To"
           />
         </div>
-      </div>
-      <div className="result">
-        <button
+      </CurrenciesContainer>
+      <ResultContainer>
+        <ConvertBtn
           onClick={() => {
             convert();
-          }}
-        >
+          }}>
           Convert
-        </button>
+        </ConvertBtn>
         <h2>Converted Amount:</h2>
         <p>{input + ' ' + from + ' = ' + output.toFixed(2) + ' ' + to}</p>
-      </div>
-    </div>
+      </ResultContainer>
+    </MainSection>
   );
 }
 
 export default App;
-
---
+++ src/App.styled.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+
+export const MainSection = styled.main`
+  min-height: 100vh;
+  display: grid;
+  place-content: center;
+  gap: 50px;
+  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+`;
+
+export const CurrenciesContainer = styled.section`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
+
+export const Input = styled.input`
+  padding: 7px;
+  font-size: 20px;
+  outline: none;
+  color: forestgreen;
+  border: none;
+  &:focus {
+    box-shadow: 0 0 3px currentColor;
+  }
+`;
+
+export const ResultContainer = styled.section`
+  font-size: 23px;
+  color: forestgreen;
+`;
+
+export const ConvertBtn = styled.button`
+  display: grid;
+  place-content: center;
+  width: 100px;
+  height: 30px;
+  border: none;
+  border: 2px solid currentColor;
+  font-weight: bold;
+  color: currentColor;
+  background-color: rgb(226, 252, 184);
+  cursor: pointer;
+`;
